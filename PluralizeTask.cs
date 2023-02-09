22e4aacc1e1defa7e5d2db056d94fc47aa8baa9c
user <user@K218PC9>
2023-02-09 11:48:12 +0300

    Добавьте файлы проекта.

---
+++ PluralizeTask.cs
@@ -0,0 +1,25 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+            int a = count % 10;
+            int b = (count / 10) % 10;
+
+            if (b == 1)
+            {
+                return "рублей";
+            }
+            if (a > 1 & a <= 4)
+            {
+                return "рубля";
+            }
+            if (a == 1)
+            {
+                return "рубль";
+            }
+            return "рублей";
+        }
+	}
+}
